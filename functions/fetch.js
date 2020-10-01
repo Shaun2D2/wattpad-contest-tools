ae883c658c92ef483eb2185c1216038209ba971f
Shaun2D2 <shaun.lobsinger@gmail.com>
2020-10-01 03:26:33 +0000

    WIP, script is working good.

---
+++ functions/fetch.js
@@ -0,0 +1,67 @@
+const got = require('got');
+const _ = require('lodash');
+const fs = require('fs');
+
+const fetch = async () => {
+
+    const bookComments = {}
+
+    const listId = 920891761;
+
+    const textSearch = '#WP2020';
+
+    try {
+        const response = await got(
+            `https://www.wattpad.com/api/v3/lists/${listId}/stories?fields=id%2Cname%2Cstories%28id%2Ctitle%2Ccover%2Cdescription%2Curl%2CvoteCount%2CreadCount%2CnumParts%2CfirstPartId%2Ccompleted%2Ctags%2Cmature%2Cuser%28name%2Cavatar%29%29%2Ctotal%2CnextUrl&limit=100`
+        ).json();
+
+        const storyIds = response.stories.map(story => story.id);
+
+        const promises = [];
+
+        storyIds.map(id => promises.push(got(`https://www.wattpad.com/api/v3/stories/${id}`).json()));
+
+        const storyParts = await Promise.all(promises);
+
+        const allThePromises = [];
+
+        for (const story of storyParts) {
+            bookComments[story.title] = {};
+
+            const commentPromises = [];
+
+            story.parts.forEach(part => commentPromises.push(got(`https://www.wattpad.com/v4/parts/${part.id}/comments?limit=1000`).json()));
+
+            const partComments = await Promise.all(commentPromises);
+
+            for (const comments of partComments) {
+
+                let commented = [];
+                
+                comments.comments.forEach(comment => {
+                    if (!new RegExp(textSearch).test(comment.body) || commented.includes(comment.author.name)) return;
+
+                    commented.push(comment.author.name);
+
+                    if (!bookComments[story.title][comment.author.name]) {
+                        bookComments[story.title][comment.author.name] = 1
+                        return;
+                    }
+
+                    bookComments[story.title][comment.author.name] = bookComments[story.title][comment.author.name] + 1;
+
+                })
+            }
+        }
+
+        fs.writeFileSync(`./output/${listId}.txt`, JSON.stringify(bookComments));
+
+        console.log(bookComments);
+
+    } catch(e) {
+        console.log(e);
+    }
+
+}
+
+module.exports = fetch;
---
+++ index.js
@@ -0,0 +1,9 @@
+const fetch = require('./functions/fetch');
+
+async function thing() {
+
+    await fetch();
+
+};
+
+thing();
