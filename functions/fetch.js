63a248988b4762a7e67773f90893b06113467675
Shaun2D2 <shaun.lobsinger@gmail.com>
2020-10-03 22:36:45 +0000

    updating function structure.

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,44 +1,45 @@
+/* eslint-disable no-restricted-syntax */
 const got = require('got');
 const _ = require('lodash');
 const fs = require('fs');
 
+const BASE_API_V3 = 'https://www.wattpad.com/api/v3/';
+const BASE_API_V4 = 'https://www.wattpad.com/v4/';
+
+const fetchList = async ({ id }) => got(
+  `${BASE_API_V3}/lists/${id}/stories?fields=id%2Cname%2Cstories%28id%2Ctitle%2Ccover%2Cdescription%2Curl%2CvoteCount%2CreadCount%2CnumParts%2CfirstPartId%2Ccompleted%2Ctags%2Cmature%2Cuser%28name%2Cavatar%29%29%2Ctotal%2CnextUrl&limit=1000`,
+).json();
+
+const fetchStory = async ({ id }) => got(`${BASE_API_V3}stories/${id}?limit=1000`).json();
+
+const fetchStoryPartComments = async ({ id }) => got(`${BASE_API_V4}parts/${id}/comments?limit=1000`).json();
+
 const fetch = async (event, context, callback) => {
-  const id = /\d+/.exec(event.path);
-
-  if (!id) {
-    callback(null, {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'id is required must be a number',
-      }),
-    });
-
-    return;
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  });
-
-  return;
-
-  const bookComments = {};
-
-  const listId = 920891761;
-
-  const textSearch = '#WP2020';
-
   try {
-    const response = await got(
-      `https://www.wattpad.com/api/v3/lists/${listId}/stories?fields=id%2Cname%2Cstories%28id%2Ctitle%2Ccover%2Cdescription%2Curl%2CvoteCount%2CreadCount%2CnumParts%2CfirstPartId%2Ccompleted%2Ctags%2Cmature%2Cuser%28name%2Cavatar%29%29%2Ctotal%2CnextUrl&limit=1000`,
-    ).json();
+    const id = _.get(/\d+/.exec(event.path), 0, null);
+
+    const hash = _.get(event.queryStringParameters, 'hash', null);
+
+    if (!id) {
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'id is required must be a number',
+        }),
+      });
+
+      return;
+    }
+
+    const bookComments = {};
+
+    const response = await fetchList({ id });
 
     const storyIds = response.stories.map((story) => story.id);
 
     const promises = [];
 
-    storyIds.map((id) => promises.push(got(`https://www.wattpad.com/api/v3/stories/${id}?limit=1000`).json()));
+    storyIds.map((storyId) => promises.push(fetchStory({ id: storyId })));
 
     const storyParts = await Promise.all(promises);
 
@@ -49,7 +50,7 @@ const fetch = async (event, context, callback) => {
 
       const commentPromises = [];
 
-      story.parts.forEach((part) => commentPromises.push(got(`https://www.wattpad.com/v4/parts/${part.id}/comments?limit=1000`).json()));
+      story.parts.forEach((part) => commentPromises.push(fetchStoryPartComments({ id: part.id })));
 
       const partComments = await Promise.all(commentPromises);
 
@@ -57,7 +58,7 @@ const fetch = async (event, context, callback) => {
         const commented = [];
 
         comments.comments.forEach((comment) => {
-          if (!new RegExp(textSearch).test(comment.body) || commented.includes(comment.author.name)) return;
+          if ((hash && !new RegExp(hash, 'i').test(comment.body)) || commented.includes(comment.author.name)) return;
 
           commented.push(comment.author.name);
 
@@ -71,10 +72,16 @@ const fetch = async (event, context, callback) => {
       }
     }
 
-    fs.writeFileSync(`./output/${listId}.txt`, JSON.stringify(bookComments));
-
-    console.log(bookComments);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(bookComments),
+    });
   } catch (e) {
+    callback(null, {
+      statusCode: 400,
+      body: e.message,
+    });
+
     console.log(e);
   }
 };
