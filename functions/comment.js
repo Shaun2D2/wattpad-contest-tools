f8b036c62d19c92f8a9cf8055bc10104e9310021
Shaun2D2 <shaun.lobsinger@gmail.com>
2020-10-03 23:01:51 +0000

    added additional functions.

---
+++ functions/comment.js
@@ -0,0 +1,28 @@
+const got = require('got');
+const _ = require('lodash');
+
+const BASE_API_V4 = 'https://www.wattpad.com/v4/';
+
+const fetchStoryPartComments = async ({ id }) => got(`${BASE_API_V4}parts/${id}/comments?limit=1000`).json();
+
+const list = async (event, context, callback) => {
+  const id = _.get(/\d+/.exec(event.path), 0, null);
+
+  if (!id) {
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'id is required must be a number',
+      }),
+    });
+  }
+
+  const response = await fetchStoryPartComments({ id });
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(response.stories),
+  });
+};
+
+exports.handler = list;
---
+++ functions/story.js
@@ -0,0 +1,34 @@
+const got = require('got');
+const _ = require('lodash');
+
+const BASE_API_V3 = 'https://www.wattpad.com/api/v3/';
+
+const fetchStory = async ({ id }) => got(`${BASE_API_V3}stories/${id}?limit=1000`).json();
+
+const list = async (event, context, callback) => {
+  try {
+    const id = _.get(/\d+/.exec(event.path), 0, null);
+
+    if (!id) {
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'id is required must be a number',
+        }),
+      });
+    }
+
+    const response = await fetchStory({ id });
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response.stories),
+    });
+  } catch (e) {
+    console.log(e);
+
+    callback(null { statusCode: 400, body: e.message });
+  }
+};
+
+exports.handler = list;
