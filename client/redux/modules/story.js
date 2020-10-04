9ff16d1b3f1a6db8b70fe80ee8064d5da537cf38
Shaun2D2 <shaun.lobsinger@gmail.com>
2020-10-04 00:23:29 +0000

    fixed transformers

--- client/redux/modules/story.js
+++ client/redux/modules/story.js
@@ -4,7 +4,7 @@ import requestAdapter from '../fetch-adapter';
 const desc = {
   getList: {
     url: 'https://fluffyduck.tech/.netlify/functions/story/:id',
-    dataTransform: (data) => JSON.stringify(data),
+    dataTransform: (data) => JSON.parse(data),
   },
 };
 
